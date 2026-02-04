cf234ab9e84a353f5d8668cd1b6f2092afd2d2db
Osunbor Destiny <osunborehigie@gmail.com>
2026-02-04 22:31:38 +0100

    Deleted the background component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,15 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedApartments from '../components/FeaturedApartments'
 import ComplimentaryAccess from '../components/ComplimentaryAccess'
-import PageBackground from '../components/PageBackground'
+
 
 const Home = () => {
   return (
     <div>
       <>
-      <PageBackground>
         <Hero />
       <FeaturedApartments />
       <ComplimentaryAccess />
-      </PageBackground>
-
       </>
     </div>
   )
