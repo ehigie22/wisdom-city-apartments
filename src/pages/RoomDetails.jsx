a7c2ad8ec1bba52dbfd2f816aa0cbd954e38bea6
Osunbor Destiny <osunborehigie@gmail.com>
2026-02-05 09:25:58 +0100

    Removed the facilityicons

--- src/pages/RoomDetails.jsx
+++ src/pages/RoomDetails.jsx
@@ -70,14 +70,14 @@ const RoomDetails = () => {
             Experience Luxury Like Never Before
           </h1>
 
-          <div className='flex flex-wrap items-center mt-3 mb-6 gap-4'>
+          {/* <div className='flex flex-wrap items-center mt-3 mb-6 gap-4'>
             {apartment.amenities.map((item, index) => (
               <div key={index} className='flex items-center gap-2 px-3 py-2 rounded-lg bg-gray-100'>
                 <img className='w-5 h-5' src={facilityIcons[item]} alt={item} />
                 <p className='text-xs'>{item}</p>
               </div>
             ))}
-          </div>
+          </div> */}
         </div>
 
         {/* <p className='text-2xl font-medium'>
