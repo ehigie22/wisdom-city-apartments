89d58e76b5cf6ab1ca47d176361f9baf13f79c6e
Osunbor Destiny <osunborehigie@gmail.com>
2026-02-08 11:56:56 +0100

    Updated the scroll to Top

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,27 @@ import AllApartments from './pages/AllApartments'
 import RoomDetails from './pages/RoomDetails'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import ScrollToTop from './components/ScrollToTop'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+
+      {/* 👇 THIS FIXES THE SCROLL ISSUE */}
+      <ScrollToTop />
+
       <div className='min-h-[70vh]'>
-      <Routes>
-        <Route path='/' element ={<Home />}/>
-        <Route path='/apartments' element ={<AllApartments />}/>
-        <Route path='/apartments/:id' element ={<RoomDetails />}/>
-        <Route path='/about' element ={<About />}/>
-        <Route path="/contact" element={<Contact />} /> 
-      </Routes>
-      </div> 
-      <Footer /> 
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/apartments' element={<AllApartments />} />
+          <Route path='/apartments/:id' element={<RoomDetails />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </div>
+
+      <Footer />
     </div>
   )
 }
