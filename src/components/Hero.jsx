e26728a9d4dc8fcf70f46a78bc463d86de9b6efb
Osunbor Destiny <osunborehigie@gmail.com>
2026-03-23 16:04:19 +0100

    Updated the heroImage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -75,7 +75,7 @@ const Hero = () => {
       bg-no-repeat bg-cover bg-center"
     >
       {/* Overlay */}
-      <div className="absolute inset-0 bg-black/65"></div>
+      <div className="absolute inset-0 bg-black/70"></div>
 
       {/* Content */}
       <div className="relative z-10">
